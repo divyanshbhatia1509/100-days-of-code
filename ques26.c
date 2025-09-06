31307895a7f710ac5cd2bc51160257fe28e8424f
divyanshbhatia1509 <divyanshbhatiabjr@gmail.com>
2025-09-07 00:04:53 +0530

    Add files via upload

---
+++ ques26.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    printf("Numbers from 1 to %d are: ", n);
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+    return 0;
+}
