52a59f0a6f92eaa176cc5ca74d8a2b74c1c7dc02
divyanshbhatia1509 <divyanshbhatiabjr@gmail.com>
2025-10-13 11:16:01 +0530

    ques13.c

--- ques13.c
+++ ques13.c
@@ -5,7 +5,8 @@ int main() {
     char ch;
     printf("Enter a character: ");
     scanf("%c", &ch);
-
+    if ((ch>='a'&& ch<='z' )||(ch>='A' && ch<='Z') )
+    {
     if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
         ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
         printf("%c is a vowel.\n", ch);
@@ -14,6 +15,14 @@ int main() {
     {
         printf("%c is a consonant.\n", ch);
     }
+}
+ else
+ {
+    if (ch>=0 && ch<=9)
+    printf("The input character is neither a consonant nor a vowel but a digit");
+    else 
+     printf("The input character is neither a consonant nor a vowel but a Special Character");
+ }   
 
     return 0;
 }
